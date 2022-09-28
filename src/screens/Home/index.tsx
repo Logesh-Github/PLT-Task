81d0d66c0da9ad903cea7d0e0cfe3ae13901d53d
Logesh-Github <logeshselva94@gmail.com>
2022-09-29 00:17:51 +0530

    Created a custom loader reusable component.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,13 +2,15 @@ import React, {useState, FunctionComponent} from 'react';
 import {View, Text, FlatList} from 'react-native';
 import styles from './styles';
 import {useNavigation} from '@react-navigation/native';
+import {CustomLoader} from '../../components/reusable-components';
 
 const Home: FunctionComponent = (): JSX.Element => {
   const navigation = useNavigation();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   return (
     <View style={styles.mainContainer}>
+      {isLoading && <CustomLoader />}
       <Text>This is home page</Text>
     </View>
   );
