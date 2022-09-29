62d61c5053e8b5e32df994ca75a15a5ed9f9a474
Logesh-Github <logeshselva94@gmail.com>
2022-09-29 18:49:37 +0530

    Created product card ui for cart screen.

--- src/screens/Cart/index.tsx
+++ src/screens/Cart/index.tsx
@@ -1,15 +1,64 @@
 import React, {useState, FunctionComponent} from 'react';
-import {View, Text, FlatList} from 'react-native';
+import {View, Text, FlatList, TouchableOpacity, Image} from 'react-native';
 import styles from './styles';
 import {useNavigation} from '@react-navigation/native';
+import {useAppDispatch, useAppSelector} from '../../hooks';
+import Images from '../../assets/images';
+import {useNetInfo} from '../../hooks/NetInfo';
+import {ProductsListInterface} from '../../redux-store/reducers/cart.reducer';
 
 const Cart: FunctionComponent = (): JSX.Element => {
   const navigation = useNavigation();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
+  const isOnline: boolean | null = useNetInfo();
+  const isLoading: boolean = useAppSelector(
+    (state: any) => state.cart.isLoading,
+  );
+  const productList: ProductsListInterface[] = useAppSelector(
+    (state: any) => state.cart.productList,
+  );
+  const cartList: ProductsListInterface[] = useAppSelector(
+    (state: any) => state.cart.cartList,
+  );
 
   return (
     <View style={styles.mainContainer}>
-      <Text>This is Cart page</Text>
+      <View style={styles.card}>
+        <View style={styles.imgView}></View>
+        <View style={styles.detailsView}>
+          <Text numberOfLines={3} style={styles.txt}>
+            {'Stone Ribbed Strappy Cut Out Detail Bodycon Dress'}
+          </Text>
+          <Text style={styles.txt}>{`$4`}</Text>
+          <Text style={styles.txt}>{`colour: black`}</Text>
+          <TouchableOpacity activeOpacity={0.7} style={styles.deleteBtn}>
+            <Image
+              source={Images.delete}
+              style={styles.img}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.7} style={styles.counterBtn}>
+            <View style={styles.iconView}>
+              <Image
+                source={Images.decrement}
+                style={styles.img}
+                resizeMode="contain"
+              />
+            </View>
+
+            <Text style={{fontSize: 18}}>{'1'}</Text>
+
+            <View style={styles.iconView}>
+              <Image
+                source={Images.increment}
+                style={styles.img}
+                resizeMode="contain"
+              />
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
--- src/screens/Cart/styles.ts
+++ src/screens/Cart/styles.ts
@@ -1,11 +1,56 @@
 import { StyleSheet } from 'react-native';
-import {Colors} from '../../utils';
+import { Colors, DeviceConfig } from '../../utils';
+import { normalize } from '../../utils/FontsCalculation';
 
 const styles = StyleSheet.create({
     mainContainer: {
         flex: 1,
-        backgroundColor: Colors.backgroundColor
-    }
+        backgroundColor: Colors.backgroundColor,
+        padding: 10
+    },
+    card: {
+        // height: DeviceConfig.screenHeight / 4,
+        width: '100%',
+        backgroundColor: Colors.white,
+        borderWidth: 1,
+        borderColor: Colors.lightGrey,
+        flexDirection: 'row'
+    },
+    imgView: {
+        width: '35%',
+    },
+    detailsView: {
+        width: '65%',
+        padding: 10,
+    },
+    img: {
+        height: '100%',
+        width: '100%'
+    },
+    txt: {
+        fontSize: normalize(16),
+        marginBottom: 5
+    },
+    deleteBtn: {
+        height: 50,
+        width: 50,
+        alignSelf: 'flex-end',
+        marginRight: 15,
+        backgroundColor: 'red'
+    },
+    counterBtn: {
+        height: 40,
+        width: 150,
+        alignSelf: 'flex-end',
+        marginRight: 15,
+        backgroundColor: '#DDDBDA',
+        marginTop: 10,
+        borderRadius: 40 / 2,
+        justifyContent: 'space-evenly',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    iconView: { height: 30, width: 30 }
 });
 
 export default styles;
